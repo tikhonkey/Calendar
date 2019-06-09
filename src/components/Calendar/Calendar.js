093e7b662233806cf2be24ae7fba07dab410f53b
Tikhon A. Koktsyh <tikhonkey@mail.ru>
2019-06-09 18:13:24 +0300

    Components update

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-// import classes from './Calendar.css'
+import './Calendar.css'
 
 import Controls from './Controls/Controls'
 import Schedule from './Schedule/Schedule'
@@ -9,8 +9,21 @@ class Calendar extends Component {
     render() {
         return (
             <React.Fragment>
+
                 <Controls />
+
+                <section className="week-days">
+                    <div>Monday</div>
+                    <div>Tuesday</div>
+                    <div>Wednesday</div>
+                    <div>Thursday</div>
+                    <div>Friday</div>
+                    <div>Saturday</div>
+                    <div>Sunday</div>
+                </section>
+
                 <Schedule />
+
             </React.Fragment>
         );
     }
--- src/components/Calendar/Controls/Controls.js
+++ src/components/Calendar/Controls/Controls.js
@@ -1,12 +1,39 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-// import classes from './Controls.css'
+import './Controls.css'
 
 const Controls = () => {
 
-    const controlsState = useState('1');
+    return (
+        <header>
 
-    return <div> Controls {controlsState[0]} </div>
+            <form name="month" className="month">
+                <select onChange="changeMonth(this.value)">
+                    <option value='0'>January</option>
+                    <option value='1'>February</option>
+                    <option value='2'>March</option>
+                    <option value='3'>April</option>
+                    <option value='4'>May</option>
+                    <option value='5'>June</option>
+                    <option value='6'>July</option>
+                    <option value='7'>August</option>
+                    <option value='8'>September</option>
+                    <option value='9'>October</option>
+                    <option value='10'>November</option>
+                    <option value='11'>December</option>
+                </select>
+            </form>
+
+            <form name="year" className="year">
+                <select onChange="changeYear(this.value)">
+                    <option name='y2018' value="2018">2018</option>
+                    <option name='y2019' value="2019">2019</option>
+                    <option name='y2020' value="2020">2020</option>
+                </select>
+            </form>
+
+        </header>
+    )
 };
 
 export default Controls;
